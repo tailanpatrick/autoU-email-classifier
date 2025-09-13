94b82674a9fbc66d002c22cba45412e75df10a8a
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-09-13 18:08:52 -0300

    renomeando arquivos

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,52 +1,12 @@
-from dotenv import load_dotenv
 import os
 from fastapi import FastAPI, UploadFile, File, Form, HTTPException
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from PyPDF2 import PdfReader
 import io
-import openai
 
-load_dotenv()  
-openai.api_key = os.getenv("OPENAI_API_KEY")
+from api.model import generate_response, classify_email
 
-
-# Classifica o email em Produtivo ou Improdutivo
-def classify_email(text: str) -> str:
-    prompt = f"""
-Classifique este email como 'Produtivo' ou 'Improdutivo'.
-Regras:
-- Produtivo: Emails que requerem uma ação ou resposta específica (ex.: solicitações de suporte técnico, atualização sobre casos em aberto, dúvidas sobre o sistema).
-- Improdutivo: Emails que não necessitam de uma ação imediata (ex.: mensagens de felicitações, agradecimentos).
-Email: "{text}"
-Responda apenas com a palavra Produtivo ou Improdutivo.
-"""
-    response = openai.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": prompt}],
-        max_tokens=10
-    )
-    categoria = response.choices[0].message.content.strip()
-    return categoria
-
-
-# Gera uma resposta automática para o email com base na categoria e texto.
-def generate_response(category: str, text: str) -> str:
-    if category == "Produtivo":
-        prompt = f"O email: {text}\n\nFoi classificado como Produtivo. Gere uma resposta curta e profissional para o remetente."
-    else:
-        prompt = f"O email: {text}\n\nFoi classificado como Improdutivo. Gere uma resposta curta e educada para o remetente."
-
-    prompt += " A resposta deve ter no máximo 50 palavras, não cortar palavras, e NÃO deve incluir '[Seu nome]' em hipótese alguma. E NÂO incluir se é PRODUTIVO OU IMPRODUTIVO"
-
-    response = openai.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": prompt}],
-        max_tokens=250
-    )
-    return response.choices[0].message.content.strip()
-
-load_dotenv()
 app = FastAPI()
 
 app.add_middleware(
